059d68581b20b5687a78cb0d8766d60fdfe60995
Ferdy Isma <fetot@Ferdys-MacBook-Air.local>
2020-10-26 17:18:03 +0700

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
+import React, { Component   } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+
+    var content = "";
+    var n = 1;
+    for(var i = 1; i<=10; i++){
+      n = n * i;
+      if((i%2) == 0){
+        content += "<tr style='text-align:center'><td>" + i + "!</td><td>" + n + "</td></tr>";
+      }else{
+        content += "<tr style='background-color:lightgrey; text-align:center'><td>" + i + "!</td><td>" + n + "</td></tr>";
+      }
+    }
+
+    return (
+      <div>
+        <table style={{borderCollapse:'collapse', width:500, textAlign: 'center', marginTop: '40px'}} align="center" border="1">
+          <thead>
+            <tr><th>Faktorial</th><th>Hasil</th></tr>
+          </thead>
+          <tbody dangerouslySetInnerHTML={{__html : content}}></tbody>
+        </table> 
+      </div>
+    );
+  }
 }
 
 export default App;
